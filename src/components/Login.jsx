afc2139e17fa835a481e7521fc6b0822bce2ce84
chris <chrisshayo4@gmail.com>
2023-09-09 06:39:47 -0700

    ecommerce frontend v1 p4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,46 @@
-import { Link } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import Navbar from './Navbar';
+import { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 export default function Login() {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [error,setError]=useState(false);
+    const [msg,setmsg]=useState(false);
+    const handlesubmit=(e)=>{
+        e.preventDefault();
+        console.log("hello");
+        if(email !== "hsgfweiudeiw0"){
+            setError(true);
+            setmsg("Invalid credentials");
+        }
+    }
   return (
     <div>
         <Navbar/>
         <div className='w-full flex justify-center mt-16'>
             <div className="container-sm bg-white p-2 rounded-lg"> 
-                <form action="" className='p-2'>
-                    <h2 className="h5">Sign In</h2>
+                <form action="" className='p-2' onSubmit={handlesubmit}>
+                    <h2 className="h5 mb-2">Sign In</h2>
+                    {error &&
+                        <div className="bg-red-200 px-4 py-2 rounded-lg flex justify-between items-center">
+                            <p>{msg}</p>
+                            <AiOutlineClose onClick={()=>setError(false)} className='w-4 h-4'/>
+                        </div>
+                    }
+                    
                     <div className="my-3">
                         {/* <label htmlFor="" className='font-semibold inline-block'>Email</label> */}
-                        <input type="email" className="input text-sm" placeholder='enter your email'required/>
+                        <input type="email" className="input text-sm" placeholder='enter your email'required
+                        value={email} onChange={(e)=>setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="my-3">
                         {/* <label htmlFor="" className='font-semibold inline-block'>Password</label> */}
-                        <input type="password" className="input text-sm" placeholder='enter your password'required/>
+                        <input type="password" className="input text-sm" placeholder='enter your password'required
+                        value={password} onChange={(e)=>setPassword(e.target.value)}
+                        />
                     </div>
                     <div className="my-3">
                         <Link className='text-sm'>Forgot password?</Link>
